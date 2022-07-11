9cda6b03717dcac15ec33775e213d6b83396c6e9
ibarretom <igorbm.23@gmail.com>
2022-07-11 20:26:42 -0300

    Pasta data e fontes do projeto

---
+++ src/data/questions.js
@@ -0,0 +1,172 @@
+export const anxietyQuestions = {
+  score: 6,
+  questions: [
+  {
+    text: "Olá, vamos saber como você se sente na escola, e com os amiguinhos (as)?",
+    classification: "startText",
+    answares: [
+      {
+        text: "sim",
+        point: 0
+      },
+      {
+        text: "não",
+        point: 0
+      }
+    ]
+  },
+  {
+    text: "Você já sentiu seu coração batendo rápido, mesmo parado(a) sem fazer nada?",
+    classification: "question",
+    answares: [
+      {
+        text: "sim",
+        point: 1
+      },
+      {
+        text: "não",
+        point: 0
+      }
+    ]
+  },
+  {
+    text: "Você já se sentiu um pouco tonto(a) ou zonzo(a)?",
+    classification: "question",
+    answares: [
+      {
+        text: "sim",
+        point: 1
+      },
+      {
+        text: "não",
+        point: 0
+      }
+    ]
+  },
+  {
+    text: "Você sente medo de vir para escola ou sair de casa?",
+    classification: "question",
+    answares: [
+      {
+        text: "sim",
+        point: 1
+      },
+      {
+        text: "não",
+        point: 0
+      }
+    ]
+  },
+  {
+    text: "Você se assusta fácil?",
+    classification: "question",
+    answares: [
+      {
+        text: "sim",
+        point: 1
+      },
+      {
+        text: "não",
+        point: 0
+      }
+    ]
+  },
+  {
+    text: "Você se sente nervoso às vezes?",
+    classification: "question",
+    answares: [
+      {
+        text: "sim",
+        point: 1
+      },
+      {
+        text: "não",
+        point: 0
+      }
+    ]
+  },
+  {
+    text: "Você sente falta de ar quando está nervoso(a)?",
+    classification: "question",
+    answares: [
+      {
+        text: "sim",
+        point: 1
+      },
+      {
+        text: "não",
+        point: 0
+      }
+    ]
+  },
+  {
+    text: "Você tem dor de barriga quando fica nervoso(a)?",
+    classification: "question",
+    answares: [
+      {
+        text: "sim",
+        point: 1
+      },
+      {
+        text: "não",
+        point: 0
+      }
+    ]
+  },
+  {
+    text: "Você fica suando mesmo sem calor?",
+    classification: "question",
+    answares: [
+      {
+        text: "sim",
+        point: 1
+      },
+      {
+        text: "não",
+        point: 0
+      }
+    ]
+  },
+  {
+    text: "Você tem falta de apetite (come pouco ou sem vontade)?",
+    classification: "question",
+    answares: [
+      {
+        text: "sim",
+        point: 1
+      },
+      {
+        text: "não",
+        point: 0
+      }
+    ]
+  },
+  {
+    text: "Você tem muita fome e quer comer sem parar?",
+    classification: "question",
+    answares: [
+      {
+        text: "sim",
+        point: 1
+      },
+      {
+        text: "não",
+        point: 0
+      }
+    ]
+  },
+  {
+    text: "Valeu!!",
+    classification: "lastQuestion",
+    answares: [
+      {
+        text: "sim",
+        point: 0
+      },
+      {
+        text: "não",
+        point: 0
+      }
+    ]
+  },
+]}
