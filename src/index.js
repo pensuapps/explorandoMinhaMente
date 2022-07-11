5936eaa9a4c39b02d82d2943b7b86d7d68c57b53
ibarretom <igorbm.23@gmail.com>
2022-07-11 20:23:27 -0300

    Adição de App.js e index.js

--- src/index.js
+++ src/index.js
@@ -1,17 +1,29 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import "./css/global.css";
+import reportWebVitals from "./reportWebVitals";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+import { AnxietyGamePage } from "./views/AnxietyGamePage";
+import { AnxietyInfoPage } from "./views/AnxietyInfoPage";
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
 root.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<App />} />
+        <Route path="/jogodaansiedade" element={<AnxietyGamePage />} />
+        <Route
+          path="/informacoessobreansiedade"
+          element={<AnxietyInfoPage />}
+        />
+      </Routes>
+    </BrowserRouter>
   </React.StrictMode>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
