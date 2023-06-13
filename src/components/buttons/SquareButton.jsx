3e614b8c0f3abe88add1a3161724948fdfe0e693
ibarretom <igorbm.23@gmail.com>
2023-06-12 23:00:21 -0300

    Tentativa 1 de corrigir o link

--- src/components/buttons/SquareButton.jsx
+++ src/components/buttons/SquareButton.jsx
@@ -2,7 +2,7 @@ import "../../css/squareButton.modules.css";
 
 export function SquareButton({ path, src, imgAltLabel, label, to = "" }) {
   return (
-    <a href={path} target="_blank">
+    <a href={path} target="_blank" rel="noreferrer">
       <button className="square-button">
         <img src={src} alt={imgAltLabel} />
         <h3>{label}</h3>
